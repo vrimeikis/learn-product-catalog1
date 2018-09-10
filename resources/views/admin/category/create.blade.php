5b765dd81305c806cf16b4999e2323d74b31279c
Gintare B <gintare.b@gmail.com>
2018-09-10 19:07:14 +0300

    fixed create blade

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -40,7 +40,7 @@
                                 <label>{{ __('Active') }}</label>
                                 <br>
                                 <label for="active">
-                                    <input id="active" type="checkbox" name="active" value="0"
+                                    <input id="active" type="checkbox" name="active" value="1"
                                     > {{ __('Check if active') }}
                                 </label>
                                 @if($errors->has('active'))
