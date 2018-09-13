66003b6da8cd037c9effdb9ede431da391fa1fed
transformeriosurudijebatai <julius.baltusis@gmail.com>
2018-09-13 18:57:13 +0300

    Prideti visur cancel mygtukai, ir isskaidyti i sonus, product bleide pakeista is new category i new product, user bleidas konkreciai pakeistas.

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -16,55 +16,57 @@
                         @endif
 
 
-    <form action="{{ route('admin.products.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
+                        <form action="{{ route('admin.products.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
 
 
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Title:</strong>
-                    <input type="text" name="title" class="form-control" placeholder="Title">
-                </div>
-            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Title:</strong>
+                                        <input type="text" name="title" class="form-control" placeholder="Title">
+                                    </div>
+                                </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Price:</strong>
-                    <input type="text" name="price" class="form-control" placeholder="Price">
-                </div>
-            </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Price:</strong>
+                                        <input type="text" name="price" class="form-control" placeholder="Price">
+                                    </div>
+                                </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Context:</strong>
-                    <textarea class="form-control" style="height:150px" name="context" placeholder="context"></textarea>
-                </div>
-            </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Context:</strong>
+                                        <textarea class="form-control" style="height:150px" name="context"
+                                                  placeholder="context"></textarea>
+                                    </div>
+                                </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <input type="file" class="form-control" name="cover"/>
-                </div>
-            </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <input type="file" class="form-control" name="cover"/>
+                                    </div>
+                                </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <input type="checkbox" id="active" name="active"
-                           value="active" />
-                    <label for="active">Active?</label>
-                </div>
-            </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <input type="checkbox" id="active" name="active"
+                                               value="active"/>
+                                        <label for="active">Active?</label>
+                                    </div>
+                                </div>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <input class="btn btn-success" type="submit" value="{{ __('Save') }}">
-                <a class="btn btn-secondary pull-right" href="javascript:history.back();">Cancel</a>
-            </div>
-        </div>
-        </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <input class="btn btn-success" type="submit" value="{{ __('Save') }}">
+                                        <a class="btn btn-secondary pull-right"
+                                           href="javascript:history.back();">Cancel</a>
+                                    </div>
+                                </div>
+                            </div>
 
-    </form>
+                        </form>
                     </div>
                 </div>
             </div>
